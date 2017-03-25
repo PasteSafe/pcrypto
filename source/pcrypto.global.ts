83b37c9fbc5b08d2c4894238db262aba77fbd02b
Chase Moskal <chasemoskal@gmail.com>
2017-03-25 12:27:04 -0700

    Add comment about the global script

--- source/pcrypto.global.ts
+++ source/pcrypto.global.ts
@@ -1,4 +1,9 @@
 
+//
+// This file merely attaches pcrypto to the browser's global namespace (onto the window).
+// Browserify is used to turn this into a bundle which can be easily included on an HTML page with a script tag.
+//
+
 import * as pcrypto from "./pcrypto"
 
 window["pcrypto"] = pcrypto
