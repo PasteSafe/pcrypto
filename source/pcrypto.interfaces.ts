8d812530a1f55add6be5e6265de23e76a222e432
Chase Moskal <lordbrimshaw@gmail.com>
2017-09-13 21:10:34 -0700

    tweak comments

--- source/pcrypto.interfaces.ts
+++ source/pcrypto.interfaces.ts
@@ -1,30 +1,30 @@
 
 /**
- * Options common to both encryption and decryption.
+ * Options common to both encryption and decryption
  */
 export interface CommonCryptionOptions {
 
-	/** Password string, which may contain any characters, but must not be an empty string (""). */
+	/** Password string, which may contain any characters, but must not be an empty string ("") */
 	password: string
 
-	/** TextEncoder charset of the plaintext. */
+	/** TextEncoder charset of the plaintext */
 	charset?: string
 }
 
 /**
- * Options for encrypting text.
+ * Options for encrypting text
  */
 export interface EncryptOptions extends CommonCryptionOptions {
 
-	/** String of text to be encrypted. */
+	/** String of text to be encrypted */
 	plaintext: string
 }
 
 /**
- * Options for decrypting text.
+ * Options for decrypting text
  */
 export interface DecryptOptions extends CommonCryptionOptions {
 
-	/** Encrypted text, which can only be decrypted with the use of the password. */
+	/** Encrypted text, which can only be decrypted with the use of the password */
 	ciphertext: string
 }
